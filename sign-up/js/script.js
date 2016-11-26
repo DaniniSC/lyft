92c13796092de0d8fb5ba5eb22bf0e15b5aeb1ef
DaniniSC <dpsantandercastro@gmail.com>
2016-11-26 13:15:36 -0300

    arreglo en campo telefónico

--- sign-up/js/script.js
+++ sign-up/js/script.js
@@ -1,1 +1,40 @@
-script.js
+var paises = [
+	{
+		nombre: "Argentina",
+		codigoTel: "+54",
+		bandera: "img/ar.png"
+	},
+	{
+		nombre: "Chile",
+		codigoTel: "+56",
+		bandera: "img/cl.png"
+	},
+	{
+		nombre: "Colombia",
+		codigoTel: "+57",
+		bandera: "img/col.png"
+	},
+	{
+		nombre: "Perú",
+		codigoTel: "+51",
+		bandera: "img/pe.jpg"
+	},
+	{
+		nombre: "Venezuela",
+		codigoTel: "+58",
+		bandera: "img/ven.jpg"
+	}
+];
+
+var paisActual = window.localStorage.getItem("paisActual") || "Chile";
+
+var pais = {};
+for (var i=0 ; i<paises.length ; ++i){
+	if (paises[i].nombre == paisActual) {
+		pais = paises[i];
+		break;
+	}
+}
+
+$('#seleccionarPais img').attr('src', pais.bandera);
+$('#codigoTel').html(pais.codigoTel);
